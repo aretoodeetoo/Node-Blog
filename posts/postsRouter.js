ec45d465676e88f8a167c79bdf81c234f04097c7
croy <roy.christie95@gmail.com>
2019-02-13 17:28:01 -0500

    Added delete functionality for posts

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -42,4 +42,18 @@ router.post('/', async (req, res) => {
     }
 })
 
+// Delete a post
+router.delete('/:id', async (req, res) => {
+    try {
+        const count = await Posts.remove(req.params.id);
+        if (count > 0){
+            res.status(200).json({ message: "Post successfully removed!"});
+        } else {
+            res.status(404).json({ message: "The post could not be deleted."});
+        }
+    } catch {
+        res.status(500).json({ message: "Error deleting this post." });
+    }
+})
+
 module.exports = router;
