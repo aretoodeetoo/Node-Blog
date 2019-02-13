60e124bdfba6dcbde9be057281007488ce598152
croy <roy.christie95@gmail.com>
2019-02-13 17:25:33 -0500

    Added ability to send a new post to the db

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -13,4 +13,33 @@ router.get('/', async (req, res) => {
     }
 })
 
+// Get post by id
+router.get('/:id', async (req, res) => {
+    try{
+        const post = await Posts.getById(req.params.id);
+        if (post){
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID could not be retrieved."});
+        }
+    } catch {
+        res.status(500).json({ message: "The post information could not be retrieved."});
+    }
+})
+
+// Add a post
+router.post('/', async (req, res) => {
+    try {
+        const { text, user_id } = req.body;
+        if (!text || !user_id){
+            res.status(400).json({ message: "Please enter both post text and username to continue"})
+        } else {
+            const newPost = await Posts.insert(req.body);
+            res.status(201).json(newPost);
+        }
+    } catch {
+        res.status(500).json({ message: "The post could not be added."});
+    }
+})
+
 module.exports = router;
